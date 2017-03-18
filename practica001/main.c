ed561018e9ceac52af7148918aebe09963d943a4
hernangomez7 <dath_math@hotmail.com>
2017-03-17 21:00:25 -0300

    Add files via upload

---
+++ practica001/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int edad;
+    printf("Write you name or die do it:\n");
+    scanf("%d",&edad);
+    printf("You have %d years",edad);
+    return 0;
+}
