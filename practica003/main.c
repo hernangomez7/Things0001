ee5b13bd69a20b9fd63d3c0d171200903a997004
hernangomez7 <dath_math@hotmail.com>
2017-03-17 22:09:35 -0300

    Add files via upload

---
+++ practica003/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("cls");
+    int nume=0;
+    printf("Write a number to choose the options\n");
+    scanf("%d",&nume);
+    switch(nume)
+    {
+        nume 1:
+        system("cls");
+        break;
+    }
+    /*
+    for(numeros=2;numeros>0;numeros--);
+    {
+      printf("Write a number or die do it:\n");
+      scanf("%d",&numero);
+      suma=suma+numero;
+
+      system("cls");
+    system("pause");
+    }
+    resultado=(suma);
+    printf("%f",resultado);
+    */
+    return 0;
+}
