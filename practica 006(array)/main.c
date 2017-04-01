9827215f80e6f5decf54e9862492711163ad73c0
hernangomez7 <dath_math@hotmail.com>
2017-03-31 22:17:39 -0300

    Add files via upload

---
+++ practica 006(array)/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num[5]=0;
+    int i;
+    int indice=0;
+    int valor=0;
+    for(i=0;i<5;i++)
+    {
+    printf("ingrese indice\n");
+    scanf("%d",&indice);
+    printf("ingrese valor\n");
+    }
+    printf("Hello world!\n");
+    printf("\n\ numero ingresado =");
+    for(int i=0;i<5;i++)
+    {
+    printf(" %d",numero[i]);
+    }
+    return 0;
+}
